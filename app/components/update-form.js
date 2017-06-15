808285c41ec78e480f6945be7a680f943d5758a6
brianontiri <foughtsentertainment@gmail.com>
2017-06-15 12:55:32 +0300

    67

--- app/components/update-form.js
+++ app/components/update-form.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewProduct: false,
+ actions: {
+   productFormShow() {
+     this.set('addNewProduct', true);
+   },
+
+   saveProduct1() {
+     var params = {
+       name: this.get('name'),
+       cost: this.get('cost'),
+       description: this.get('description'),
+       image: this.get('image'),
+
+     };
+     this.set('addNewProduct', false);
+     this.sendAction('saveProduct2', params);
+   }
+ }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,12 @@ export default Ember.Route.extend({
   model(){
     return this.store.findAll('product');
   },
-  // action:{
-  //   click()
-  //
-  // }
+  actions: {
+     saveProduct3(product,params) {
+       var newProduct = this.store.createRecord('product', params);
+       newProduct.save();
+       this.transitionTo('index');
+     }
+   }
+
 });
