911fc0012903471ae384207eafbd3c88906f6399
brianontiri <foughtsentertainment@gmail.com>
2017-06-15 10:30:27 +0300

    added new model

--- app/models/product.js
+++ app/models/product.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  cost: DS.attr('string'),
+  description: DS.attr('string'),
+  image: DS.attr('string'),
+  name: DS.attr('string')
 });
